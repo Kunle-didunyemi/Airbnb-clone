518c503cb824ba8cf3f0bf4974dd46c918d4e9bc
Senator kay <senatorkay2@gmail.com>
2023-04-05 15:50:05 +0100

    Added the input for registration

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
   id: string;
@@ -13,8 +14,50 @@ interface InputProps {
   errors: FieldErrors;
 }
 
-const Input = () => {
-  return <div></div>;
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  disabled,
+  formalPrice,
+  required,
+  register,
+  errors,
+}) => {
+  return (
+    <div className="w-full relative">
+      {formalPrice && (
+        <BiDollar
+          size={24}
+          className=" text-neutral-700 absolute top-5 left-2"
+        />
+      )}
+      <input
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=" "
+        type={type}
+        className={` peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed
+        ${formalPrice ? "pl-9" : " pl-4"}
+        ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+        ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+        `}
+      />
+      <label
+        className={`absolute
+      text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0]
+      ${formalPrice ? "left-9" : "left-4"}
+      peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+      peer-focus:scale-75
+      peer-focus:-translate-y-4
+      `}
+        htmlFor=""
+      >
+        {label}
+      </label>
+    </div>
+  );
 };
 
 export default Input;
